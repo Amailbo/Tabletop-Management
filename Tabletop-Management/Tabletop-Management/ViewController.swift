e738d00f5f8a000e0c17ef35732182a38fb2148d
Amailbo <denis_aniskov@hotmail.com>
2021-10-24 13:29:37 -0700

    init commit

---
+++ Tabletop-Management/Tabletop-Management/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tabletop-Management
+//
+//  Created by Denis Aniskov on 2021-10-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
